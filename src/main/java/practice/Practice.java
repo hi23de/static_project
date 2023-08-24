d5c5dd9b7d472be6a058d984d8be10e125c34088
HIDEAKI2023\arika <hideakibirei2023@gmail.com>
2023-08-25 00:23:57 +0900

    while first

---
+++ src/main/java/practice/Practice.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Practice {
+
+	public static void main(String args[]) {
+		PracticeWhile practicewhile = new PracticeWhile();
+		practicewhile.While(args);
+	}
+	
+}
+//メソッドの呼び出しで表示する。
---
+++ src/main/java/practice/PracticeWhile.java
@@ -0,0 +1,16 @@
+package practice;
+
+//100から1ずつ減算して、0まで表示する。
+public class PracticeWhile {
+
+	public void While(String[] args) {
+		int num = 101;
+
+		while (num > 0) {
+			num--;
+
+			System.out.println(num);
+		}
+	}
+
+}
